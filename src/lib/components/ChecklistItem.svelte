c8edf2ba5f70b04a0aaee1abce7ad9aea7e40d8c
Tarik <tarikmerrylees@gmail.com>
2023-06-26 21:03:38 +0100

    style checklist

--- src/lib/components/ChecklistItem.svelte
+++ src/lib/components/ChecklistItem.svelte
@@ -5,8 +5,8 @@
   let isChecked = false;
 </script>
 
-<div class="checklist-line">
-  <SlideToggle name="" bind:checked={isChecked} />
+<div class="checklist-line card py-2 px-5 variant-filled-secondary">
+  <SlideToggle name={item} bind:checked={isChecked} active="bg-success-500" background="bg-error-500" />
   <span class="text-xl">{item}</span>
 </div>
 
