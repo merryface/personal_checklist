cb79a320dca2bdbc6040edb1572e184c548c5707
Tarik <tarikmerrylees@gmail.com>
2023-07-01 22:00:36 +0100

    adjust styling in tour dep

--- src/routes/tour-departure/+page.svelte
+++ src/routes/tour-departure/+page.svelte
@@ -15,7 +15,7 @@
 
 </script>
 
-<main class="container h-full mx-auto flex flex-col gap-3 items-center px-5 py-10">
+<main class="container flex flex-col gap-3 items-center px-5 pt-10 py-20">
   <h1 class="text-[1.5rem] leading-[2rem] text-center mb-10">Pre-Tour Departure Checklist</h1>
   <Checklist items={checklist} />
 </main>
